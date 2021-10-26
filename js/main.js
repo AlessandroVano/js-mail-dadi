04e114541bfe990f26427fcaaf3a461b9be8cf10
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-26 14:05:22 +0200

    initialization repo ex 1 (mail)

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+/**
+ * ESERCIZIO 1 MAIL
+ * 
+ * crea una lista contenente almeno 3 indirizzi email
+ * chiedi all'utente la sua email,
+ * controlla che sia nella lista di chi può accedere (lista creata soopra al punto 1)
+ * stampa un messaggio appropiato sull'esito del controllo.
+ */
+
+
+
+//
